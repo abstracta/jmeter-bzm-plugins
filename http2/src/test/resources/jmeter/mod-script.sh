ba4ec7406a5c36fb481bc10064abbc64e138830b
Sebastián Lorenzo <sebastianlorenzo88@gmail.com>
2018-06-04 17:46:52 -0300

    fixed path

--- http2/src/test/resources/jmeter/mod-script.sh
+++ http2/src/test/resources/jmeter/mod-script.sh
@@ -1,1 +1,5 @@
-sed -i "s/java \$JVM_ARGS/java -Xbootclasspath\/p:$APLN_JAR \$JVM_ARGS /" jmeter.sh
+#!/usr/bin/env bash
+set -e
+
+JMETER_PATH=${project.basedir}/.jmeter/$JMETER_VERSION
+sed -i "s/java \$JVM_ARGS/java -Xbootclasspath\/p:$APLN_JAR \$JVM_ARGS /" JMETER_PATH/bin/jmeter.sh
