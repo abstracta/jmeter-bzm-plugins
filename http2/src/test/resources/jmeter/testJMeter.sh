a733ce1af29f4d6bea5efc519c508efcb1d0954d
Sebastián Lorenzo <sebastianlorenzo88@gmail.com>
2018-06-04 17:35:56 -0300

    fixed path

--- http2/src/test/resources/jmeter/testJMeter.sh
+++ http2/src/test/resources/jmeter/testJMeter.sh
@@ -9,7 +9,8 @@ APLN_JAR=$(ls $JMETER_TEST_PATH/lib/ | grep alpn-boot)
 JARS=$(ls $JMETER_TEST_PATH/lib/ | grep -v alpn-boot)
 cd $JMETER_TEST_PATH/lib/
 mkdir -p $JMETER_PATH/lib/ext/ && cp -f $JARS $JMETER_PATH/lib/ext/
-bzt -o modules.jmeter.path=$JMETER_PATH -o modules.jmeter.version=$JMETER_VERSION $JMETER_TEST_PATH/testJMeter.yaml || ERROR=$?
+cd $JMETER_TEST_PATH
+bzt -o modules.jmeter.path=$JMETER_PATH -o modules.jmeter.version=$JMETER_VERSION testJMeter.yaml || ERROR=$?
 cd $JMETER_PATH/lib/ext/
 rm JARS
 exit $ERROR
