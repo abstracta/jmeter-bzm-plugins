4404ff44ee67bfb32ba6916e2034453eb8689139
Sebastián Lorenzo <sebastianlorenzo88@gmail.com>
2018-06-05 15:42:47 -0300

    .

--- http2/src/test/resources/jmeter/testJMeter.sh
+++ http2/src/test/resources/jmeter/testJMeter.sh
@@ -9,6 +9,7 @@ APLN_JAR=$(ls $JMETER_TEST_PATH/lib/ | grep alpn-boot)
 JARS=$(ls $JMETER_TEST_PATH/lib/ | grep -v alpn-boot)
 cd $JMETER_TEST_PATH/lib/
 mkdir -p $JMETER_PATH/lib/ext/ && cp -f $JARS $JMETER_PATH/lib/ext/
+ls $JMETER_PATH/lib/ext/
 cd $JMETER_TEST_PATH
 chmod +x mod-script.sh
 bzt -o modules.jmeter.path=$JMETER_PATH -o modules.jmeter.version=$JMETER_VERSION testJMeter.yaml || ERROR=$?
