f8a30b0a51bd0d117bb6db7ac774d5fa7595ebb7
Sebastián Lorenzo <sebastianlorenzo88@gmail.com>
2018-06-05 14:50:18 -0300

    .

--- http2/src/test/resources/jmeter/mod-script.sh
+++ http2/src/test/resources/jmeter/mod-script.sh
@@ -1,10 +1,1 @@
-#!/usr/bin/env bash
-set -e
-
-pwd
-echo "**********mod-script********"
-echo $JMETER_PATH
-echo $JMETER_TEST_PATH
-echo $APLN_JAR
-echo "**********mod-script********"
 sed -i "s/java \$JVM_ARGS/java -Xbootclasspath\/p:$JMETER_TEST_PATH/$APLN_JAR \$JVM_ARGS /" $JMETER_PATH/bin/jmeter.sh
--- http2/src/test/resources/jmeter/testJMeter.sh
+++ http2/src/test/resources/jmeter/testJMeter.sh
@@ -5,29 +5,14 @@ export JMETER_VERSION=$1
 export JMETER_PATH=${project.basedir}/.jmeter/$JMETER_VERSION
 export JMETER_TEST_PATH=${project.basedir}/target/jmeter-test
 
-echo $JMETER_VERSION
-echo $JMETER_PATH
-echo $JMETER_TEST_PATH
-pwd
 APLN_JAR=$(ls $JMETER_TEST_PATH/lib/ | grep alpn-boot)
-echo $APLN_JAR
-pwd
 JARS=$(ls $JMETER_TEST_PATH/lib/ | grep -v alpn-boot)
-echo $JARS
-pwd
 cd $JMETER_TEST_PATH/lib/
-pwd
 mkdir -p $JMETER_PATH/lib/ext/ && cp -f $JARS $JMETER_PATH/lib/ext/
-pwd
 cd $JMETER_TEST_PATH
 chmod +x mod-script.sh
-pwd
-ls
 bzt -o modules.jmeter.path=$JMETER_PATH -o modules.jmeter.version=$JMETER_VERSION testJMeter.yaml || ERROR=$?
-pwd
 cd $JMETER_PATH/lib/ext/
-pwd
 rm $JARS
-pwd
 exit $ERROR
 
