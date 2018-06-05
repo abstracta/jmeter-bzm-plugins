c0c9dc0b5de014b626c5ffbfa4b7d2bb1f4b8548
Sebastián Lorenzo <sebastianlorenzo88@gmail.com>
2018-06-05 11:34:23 -0300

    .

--- http2/src/test/resources/jmeter/mod-script.sh
+++ http2/src/test/resources/jmeter/mod-script.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-PWD
 JMETER_TEST_PATH=${project.basedir}/target/jmeter-test
 sed -i "s/java \$JVM_ARGS/java -Xbootclasspath\/p:$APLN_JAR \$JVM_ARGS /" JMETER_TEST_PATH/bin/jmeter.sh
