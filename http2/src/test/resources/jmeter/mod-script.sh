98d53d723f73443012033337289219ebc1e388f8
Sebastián Lorenzo <sebastianlorenzo88@gmail.com>
2018-06-06 12:11:35 -0300

    .

--- http2/src/test/resources/jmeter/mod-script.sh
+++ http2/src/test/resources/jmeter/mod-script.sh
@@ -1,1 +1,1 @@
-sed -i "s?java \$JVM_ARGS?java -Xbootclasspath/p:$JMETER_TEST_PATH/$APLN_JAR \$JVM_ARGS ?" $JMETER_PATH/bin/jmeter.sh
+sed -i "s?java \$JVM_ARGS?java -Xbootclasspath/p:$JMETER_TEST_PATH/lib/alpn-boot.jar \$JVM_ARGS ?" $JMETER_PATH/bin/jmeter.sh
